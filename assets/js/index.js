08d3a1f3f6dc62a6c7da82d26d548728d5eacf2e
Oliver C <ocronk10@gmail.com>
2022-03-08 17:04:28 -0600

    Updated JS file to fit submission criteria; allows user to select if they want to include/disclude numbers, symbols, uppercase, or lowercase letters

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,38 @@
-var password=document.getElementById("password");
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 12;
-var password = "";
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("length");
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
+const incUppercase = document.getElementById("uppercase")
+const incLowercase = document.getElementById("lowercase")
+const generateBtn = document.getElementById("generate");
+
+generateBtn.addEventListener("click", () => {
+    let characters = "";
+    incNumbers.checked ? (characters += numbers) : "";
+    incSymbols.checked ? (characters += symbols) : "";
+    incUppercase.checked ? (characters += uppercase) : "";
+    incLowercase.checked ? (characters += lowercase) : "";
+    passwordTxt.value = generatePassword(length.value, characters);
+  });
+
+  const generatePassword = (length, characters) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return password;
+  };
+
+  const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", () => {
+  passwordTxt.select();
+  document.execCommand("copy");
+  alert("Password Copied");
+});
