ff4172505e1b417946df58948d129d1e3e62253a
Oliver C <ocronk10@gmail.com>
2022-03-08 16:34:58 -0600

    Set variables for parameters in which password will pull chracters from

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+var password=document.getElementById("password");
+
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
+var password = "";
